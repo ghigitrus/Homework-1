4efd241bec4533431469d65125712548df8d46a2
ghigitrus <mail2work@mail.ru>
2015-07-14 01:22:15 +0300

    Home task#1

---
+++ javascript.js
@@ -0,0 +1,10 @@
+var helloWorld = function (){
+	for(i=0; i<10; i++){
+	console.log("Hello World! ");
+	}
+}
+var pressed=0; 
+function counter() 
+{ 
+document.getElementById('count').innerHTML = ++pressed; 
+} 
